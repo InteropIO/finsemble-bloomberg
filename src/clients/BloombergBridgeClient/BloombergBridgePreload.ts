2a00c4557c1b9d4ac9683293ec2adf1a13a257e4
Tiago Pina <tiago.pina@cosaic.io>
2023-03-08 13:03:09 +0000

    Update to Finsemble 8.4.1

--- src/clients/BloombergBridgeClient/BloombergBridgePreload.ts
+++ src/clients/BloombergBridgeClient/BloombergBridgePreload.ts
@@ -1,18 +1,25 @@
-import {BloombergBridgeClient} from "./BloombergBridgeClient";
+import { types } from "@finsemble/finsemble-core";
+
+import { BloombergBridgeClient } from "./BloombergBridgeClient";
 
 /**
  * Automated setup function enabling use as preload on a Finsemble component.
  */
- const setupBloombergBridgeClient = () => {
-    console.log("Setting up BloombergBridgeClient");
-	(FSBL as any).Clients.BloombergBridgeClient = new BloombergBridgeClient((FSBL as any).Clients.Router, (FSBL as any).Clients.Logger);
-	window.dispatchEvent(new Event('BloombergBridgeClientReady'));
+const setupBloombergBridgeClient = () => {
+  console.log("Setting up BloombergBridgeClient");
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  FSBL.Clients.BloombergBridgeClient = new BloombergBridgeClient(
+    FSBL.Clients.RouterClient,
+    FSBL.Clients.Logger
+  );
+  window.dispatchEvent(new Event("BloombergBridgeClientReady"));
 };
 
 // Startup pattern for preload. Preloads can come in any order, so we need to wait on either the window event or the
 // FSBL event
-if ((window as any).FSBL && (FSBL as any).addEventListener) {
-    (FSBL as any).addEventListener("onReady", setupBloombergBridgeClient);
+if (window.FSBL && FSBL.addEventListener) {
+  FSBL.addEventListener("onReady", setupBloombergBridgeClient);
 } else {
-    window.addEventListener("FSBLReady", setupBloombergBridgeClient);
+  window.addEventListener("FSBLReady", setupBloombergBridgeClient);
 }
