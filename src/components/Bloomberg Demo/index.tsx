1046df17643eb6c04eaf480f5efe776e82270fe9
Tiago Pina <tiago.pina@cosaic.io>
2023-03-20 11:05:42 +0000

    listen to correct BloombergBridgeClientReady event

--- src/components/Bloomberg Demo/index.tsx
+++ src/components/Bloomberg Demo/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import SecurityFinder from "./SecurityFinder";
 
+window.addEventListener("BloombergBridgeClientReady", BBGReady);
+
 function BBGReady() {
   ReactDOM.render(<SecurityFinder />, document.getElementById("root"));
 }
-
-FEAGlobals.FSBL.addEventListener("onReady", BBGReady);
