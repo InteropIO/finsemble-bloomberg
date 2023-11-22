870caadf1607191793306427b828362232173341
Julianna Langston <julianna@cosaic.io>
2023-11-22 13:25:00 -0500

    Include terminal commands

--- fdc3/src/BloombergBridgeApp.tsx
+++ fdc3/src/BloombergBridgeApp.tsx
@@ -1,11 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 
+type CommandHistoryEntry = {
+  command: string;
+  tails: string;
+  security: string;
+  result: string;
+}
+
 function BloombergBridgeApp() {
   const [security, setSecurity] = useState("");
   const [groupInfo, setGroupInfo] = useState<Record<number, string>>({});
   const [selectedGroupIds, setSelectedGroupIds] = useState<number[]>([]);
   const [selectedTab, setSelectedTab] = useState(1);
+
+  const [terminalCommand, setTerminalCommand] = useState("");
+  const [terminalTails, setTerminalTails] = useState("");
+  const [commandHistory, setCommandHistory] = useState<CommandHistoryEntry[]>([]);
 
   const shouldShowGroupZeroState = Object.keys(groupInfo).length === 0;
 
@@ -42,12 +53,8 @@ function BloombergBridgeApp() {
   };
 
   const relayContextToBloomberg = (instrument: string) => {
-    console.log(selectedGroupIds);
     selectedGroupIds.forEach((groupId) => {
-      console.log("Sending context to... ", groupId, groupInfo[groupId]);
-      FSBL.Clients.BloombergBridgeClient.runSetGroupContext(groupInfo[groupId], instrument, "", (err, response) => {
-        console.log(err, response);
-      });
+      FSBL.Clients.BloombergBridgeClient.runSetGroupContext(groupInfo[groupId], instrument, "", () => {});
     });
   }
 
@@ -140,7 +147,70 @@ function BloombergBridgeApp() {
         </div>
 
         <div role="tabpanel" data-active={selectedTab === 2}>
+          {security === "" && <div id="error-warning" style={{color: "red", fontWeight: "bold"}}>Must have a security to run a command.</div>}
 
+          <table role="presentation">
+            <thead>
+              <tr>
+                <th>Command</th>
+                <th>Tails</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input value={terminalCommand} disabled={security === ""} aria-label="Command" type="text" size={5} maxLength={5} onChange={(e) => {
+                    setTerminalCommand(e.target.value);
+                  }}/>
+                </td>
+                <td>
+                  <input value={terminalTails} disabled={security === ""} aria-label="Tails" type="text" onChange={(e) => {
+                    setTerminalTails(e.target.value);
+                  }} />
+                </td>
+                <td>
+                  <button disabled={terminalCommand === ""} onClick={() => {
+                    FSBL.Clients.BloombergBridgeClient.runBBGCommand(
+                      terminalCommand,
+                      security,
+                      terminalTails,
+                      "",
+                      (err: string | null) => {
+                        console.log(1);
+                        setCommandHistory([
+                          {
+                            command: terminalCommand,
+                            security,
+                            tails: terminalTails,
+                            result: err ?? "Sucess"
+                          },
+                          ...commandHistory
+                        ]);
+                        console.log(2);
+                        setTerminalCommand("");
+                        console.log(3);
+                        setTerminalTails("");
+                        console.log(4);
+                      }
+                    )
+                  }}>Run</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          {commandHistory.length > 0 && <h2>Recent history</h2>}
+
+          <ul className="history-log">
+            {commandHistory.slice(0, 5).map((entry, index) => <li key={index}>
+              {entry.security}: <pre>{entry.command} {entry.tails}</pre><br/>
+              <b>Result:</b> {entry.result}<br/>
+              <button onClick={() => {
+                setTerminalCommand(entry.command);
+                setTerminalTails(entry.tails);
+              }}>Re-use command</button>
+            </li>)}
+          </ul>
         </div>
       </div>
     </div>
