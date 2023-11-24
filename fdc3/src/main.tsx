300cc5d4b8cc4eec8d399fa46e884975118019ee
Kris West <kris@cosaic.io>
2023-11-24 15:19:41 +0000

    Fix ready pattern to avoid races

--- fdc3/src/main.tsx
+++ fdc3/src/main.tsx
@@ -2,8 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./App.css";
 
-window.addEventListener("FSBLReady", () => {
+const FSBLReady = () => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />,
   )
-});
+};
+
+if (window.FSBL && FSBL.addEventListener) {
+  FSBL.addEventListener("onReady", FSBLReady);
+} else {
+  window.addEventListener("FSBLReady", FSBLReady);
+}
